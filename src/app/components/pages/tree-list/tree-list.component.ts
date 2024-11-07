0b55519fff863072bc83f11036531bdd87cf801a
Sonu Kumari <sonu.kumari@mapmyindia.com>
2024-11-07 23:05:01 +0530

    search issue resolve

--- src/app/components/pages/tree-list/tree-list.component.ts
+++ src/app/components/pages/tree-list/tree-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { ToastrService } from 'ngx-toastr';
+import { SearchPipe } from 'src/app/search.pipe';
 import { AuthServicesService } from 'src/app/services/auth/auth-services.service';
 
 @Component({
   selector: 'app-tree-list',
   templateUrl: './tree-list.component.html',
-  styleUrls: ['./tree-list.component.scss']
+  styleUrls: ['./tree-list.component.scss'],
+  providers: [SearchPipe]
 })
 export class TreeListComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -18,14 +20,15 @@ export class TreeListComponent implements OnInit {
   token: any;
   referralTree: any[] = [];
   filteredReferrals: any[] = [];
-  searchQuery: string = '';
+  searchTerm: string = '';
   pageSize: number = 10;
   selectedReferral: any   // Store selected referral for the modal
 
 
   constructor(
     private authService: AuthServicesService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    public searchFilterPipe: SearchPipe
   ) { }
 
   ngOnInit(): void {
@@ -39,7 +42,7 @@ export class TreeListComponent implements OnInit {
       next: (response: any) => {
         this.referralTree = response.data; // Adjust this according to actual API response
         this.pagelength = this.referralTree.length;
-        this.filterReferrals();
+        // this.filterReferrals();
         this.localPagination()
         this.loading = false;
       },
@@ -51,17 +54,16 @@ export class TreeListComponent implements OnInit {
       }
     });
   }
-
-  filterReferrals(): void {
-    const query = this.searchQuery.toLowerCase();
-    this.filteredReferrals = this.referralTree.filter(referral =>
-      referral.referralCode.toLowerCase().includes(query) ||
-      referral.name.toLowerCase().includes(query) ||
-      referral.email.toLowerCase().includes(query) ||
-      referral.mobile.toLowerCase().includes(query)
-    );
-  }
   
+  async filterReferrals() {
+    this.filteredReferrals = await this.searchFilterPipe.transform(this.referralTree, this.searchTerm);
+    this.currentPage = 1;
+    let toPage = this.itemPerPage * this.currentPage;
+    let fromPage = toPage - this.itemPerPage;
+    this.filteredReferrals = this.filteredReferrals.slice(fromPage, toPage);
+    this.paginator.pageIndex = 0;
+  }
+
   getReferralInfo(referralCode: string): void {
     this.authService.getReferralInfomation(referralCode, this.token).subscribe({
       next: (response: any) => {
@@ -75,11 +77,6 @@ export class TreeListComponent implements OnInit {
         this.toastr.error('Failed to retrieve referral data');
       }
     });
-  }
-
-  onSearchChange(): void {
-    this.filterReferrals();
-    this.currentPage = 1; // Reset to first page on new search
   }
 
   // onPageChange(event: PageEvent): void {
--- src/app/components/pages/tree-list/tree-list.module.ts
+++ src/app/components/pages/tree-list/tree-list.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TreeListRoutingModule } from './tree-list-routing.module';
 import { TreeListComponent } from './tree-list.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { FormsModule } from '@angular/forms';
-import { Ng2SearchPipeModule } from '@ngx-maintenance/ng2-search-filter';
+// import { Ng2SearchPipeModule } from '@ngx-maintenance/ng2-search-filter';
+import { SearchPipe } from 'src/app/search.pipe';
 
 
 @NgModule({
   declarations: [
-    TreeListComponent
+    TreeListComponent,
+    SearchPipe
   ],
   imports: [
     CommonModule,
     TreeListRoutingModule,
     MatPaginatorModule,
     FormsModule,
-    Ng2SearchPipeModule
-  ]
+  ],
+  exports: [SearchPipe],
+  schemas: [NO_ERRORS_SCHEMA],
 })
 export class TreeListModule { }
